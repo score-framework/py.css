9b26f9096fba85c9d9e3eebb2e63b304577907ea
Necdet Can Atesman <c.atesman@gmx.net>
2015-12-15 16:50:41 +0100

    Release: 0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.css',
-    version='0.1.2',
+    version='0.1.3',
     description='Helpers for managing css with The SCORE Framework',
     long_description=README,
     author='strg.at',
